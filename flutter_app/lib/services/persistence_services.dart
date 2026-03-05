b6265487e2ee69289b9057c1ca8234175fdd8e6b
David Adekoya <davidadekoya88@gmail.com>
2026-03-05 18:21:07 +0100

    project setu p in process

---
+++ flutter_app/lib/services/persistence_services.dart
@@ -0,0 +1,79 @@
+import '../config/pref_keys.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PersistenceService {
+  static final PersistenceService _manager = PersistenceService._internal();
+  SharedPreferences? _preferences;
+
+  factory PersistenceService() {
+    return _manager;
+  }
+
+  //This makes sure preference is loaded.
+  Future<void> _ensurePreferenceLoaded() async {
+    _manager._preferences ??= await SharedPreferences.getInstance();
+  }
+
+  Future<bool> getSignInStatus() async {
+    await _manager._ensurePreferenceLoaded();
+    return Future.value(
+      _manager._preferences!.getBool(PrefKeys.signInStatusKey) ?? false,
+    );
+  }
+
+  Future<void> saveSignInStatus(bool signInStatus) async {
+    await _manager._ensurePreferenceLoaded();
+    _manager._preferences!.setBool(PrefKeys.signInStatusKey, signInStatus);
+  }
+
+  Future<void> saveUserEmail(String userEmail) async {
+    await _manager._ensurePreferenceLoaded();
+    _manager._preferences!.setString(PrefKeys.userEmail, userEmail);
+  }
+
+  Future<String?> getUserEmail() async {
+    await _manager._ensurePreferenceLoaded();
+    return _manager._preferences!.getString(PrefKeys.userEmail);
+  }
+
+  Future<void> saveUserName(String userName) async {
+    await _manager._ensurePreferenceLoaded();
+    _manager._preferences!.setString(PrefKeys.userName, userName);
+  }
+
+  Future<String?> getUserName() async {
+    await _manager._ensurePreferenceLoaded();
+    return _manager._preferences!.getString(PrefKeys.userName);
+  }
+
+  Future<void> saveThemeMode(String themeMode) async {
+    await _manager._ensurePreferenceLoaded();
+    _manager._preferences!.setString(PrefKeys.themeMode, themeMode);
+  }
+
+  Future<String?> getThemeMode() async {
+    await _manager._ensurePreferenceLoaded();
+    return _manager._preferences!.getString(PrefKeys.themeMode);
+  }
+
+  Future<void> saveHasShownLocationRationale(bool value) async {
+    await _manager._ensurePreferenceLoaded();
+    _manager._preferences!.setBool(PrefKeys.hasShownLocationRationale, value);
+  }
+
+  Future<bool> getHasShownLocationRationale() async {
+    await _manager._ensurePreferenceLoaded();
+    return _manager._preferences!.getBool(PrefKeys.hasShownLocationRationale) ??
+        false;
+  }
+
+  Future<void> signOut() async {
+    await _manager._ensurePreferenceLoaded();
+    await _manager._preferences!.remove(PrefKeys.userEmail);
+    await _manager._preferences!.remove(PrefKeys.userUuid);
+    await _manager._preferences!.remove(PrefKeys.userName);
+    await _manager._preferences!.remove(PrefKeys.signInStatusKey);
+  }
+
+  PersistenceService._internal();
+}
