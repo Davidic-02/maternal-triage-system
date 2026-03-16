66253c41f8036ee23306b27c7f545536251cc5fc
David Adekoya <davidadekoya88@gmail.com>
2026-03-16 21:50:44 +0100

    Save local changes before pull

--- flutter_app/lib/presentation/screens/onboarding/splash_screen.dart
+++ flutter_app/lib/presentation/screens/onboarding/splash_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-class ExampleScreen extends HookWidget {
-  const ExampleScreen({super.key});
+class SplashScreen extends HookWidget {
+  const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
