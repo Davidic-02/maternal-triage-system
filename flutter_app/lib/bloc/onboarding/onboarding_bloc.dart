0c32e9602e5e1834ac525e104ab60e0b75010c38
David Adekoya <davidadekoya88@gmail.com>
2026-03-19 19:11:37 +0100

    started the onboarding

---
+++ flutter_app/lib/bloc/onboarding/onboarding_bloc.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:maternal_triage/services/persistence_services.dart';
+
+part 'onboarding_event.dart';
+part 'onboarding_state.dart';
+part 'onboarding_bloc.freezed.dart';
+
+class OnboardingBloc extends Bloc<OnboardingEvent, OnboardingState> {
+  final PersistenceService _persistenceService;
+
+  OnboardingBloc({PersistenceService? persistenceService})
+      : _persistenceService = persistenceService ?? PersistenceService(),
+        super(const OnboardingState()) {
+    on<_PageChanged>(_onPageChanged);
+    on<_TermsAccepted>(_onTermsAccepted);
+    on<_Completed>(_onCompleted);
+  }
+
+  void _onPageChanged(_PageChanged event, Emitter<OnboardingState> emit) {
+    emit(state.copyWith(currentPage: event.page));
+  }
+
+  void _onTermsAccepted(_TermsAccepted event, Emitter<OnboardingState> emit) {
+    emit(state.copyWith(hasAcceptedTerms: event.accepted));
+  }
+
+  Future<void> _onCompleted(
+    _Completed event,
+    Emitter<OnboardingState> emit,
+  ) async {
+    await _persistenceService.saveOnboardingComplete();
+    // navigation is handled by BlocListener in the screen
+  }
+}
---
+++ flutter_app/lib/bloc/onboarding/onboarding_state.dart
@@ -0,0 +1,9 @@
+part of 'onboarding_bloc.dart';
+
+@freezed
+abstract class OnboardingState with _$OnboardingState {
+  const factory OnboardingState({
+    @Default(0) int currentPage,
+    @Default(false) bool hasAcceptedTerms,
+  }) = _OnboardingState;
+}
