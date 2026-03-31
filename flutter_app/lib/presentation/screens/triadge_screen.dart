50a9beeb30acba42d22e08d0bbf1c50857d06c3a
David Adekoya <davidadekoya88@gmail.com>
2026-03-31 10:02:40 +0100

    refctoreed

--- flutter_app/lib/presentation/screens/triadge_screen.dart
+++ flutter_app/lib/presentation/screens/triadge_screen.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:lottie/lottie.dart';
+import 'package:maternal_triage/bloc/triage/triage_bloc.dart';
 
 class TriadgeScreen extends HookWidget {
-  static const routeName = '/';
+  static const routeName = '/triage';
   const TriadgeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return  Scaffold(body: 
+   SafeArea(
+        child: BlocBuilder<TriageBloc, TriageState>(
+          buildWhen: (previous, current) => _triageBuildWhen(
+            context, previous, current
+          ),
+          builder: (context, state){ return
+  const  SingleChildScrollView(
+          child: Padding(
+            padding:  EdgeInsets.symmetric(horizontal: 30),
+           
+
+
+
+
+
+
+
+    );
   }
 }
