1a3967063da1aea1b2a905a3ef510e1ac183e2c2
David Adekoya <davidadekoya88@gmail.com>
2026-04-07 17:03:22 +0100

    Stop tracking config folder

--- flutter_app/lib/config/pref_keys.dart
+++
@@ -1,7 +0,0 @@
-class PrefKeys {
-  static const userName = 'user_name';
-  static const userRole = 'user_role';
-  static const signInStatusKey = 'sign_in_status';
-  static const userEmail = 'user_email';
-  static const onboardingComplete = 'onboarding_complete';
-}
